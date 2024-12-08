4565d714d5e8d909336c48aee9d8ca8fe0fc17f1
Nitesh <nitesh.codespace@gmail.com>
2024-12-09 05:15:35 +0530

    core-interfaces

---
+++ src/main/java/com/learning/web/browser/Browser.java
@@ -0,0 +1,12 @@
+package com.learning.web.browser;
+
+public interface Browser {
+
+	//core method in the browser 
+	void navigateTo(String url);
+	void maximize();
+	void closeBrowser();
+	
+	//method ->locate Elements ||
+	
+}
